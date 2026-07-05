6bd72001cfa9373946085f853f29c733ba6e25d2
s-knk <625.somq2525@gmail.com>
2026-07-05 23:47:49 +0900

    TestFlight白画面の原因依存を修正

--- app.config.js
+++ app.config.js
@@ -1,12 +1,36 @@
-const appJson = require('./app.json');
-
 module.exports = () => {
-  const config = appJson.expo;
   return {
-    ...config,
+    name: '勝率カウンター',
+    slug: 'win-track',
+    version: '0.1.0',
+    icon: './assets/icon1.png',
+    platforms: ['ios'],
+    orientation: 'portrait',
+    scheme: 'wintrack',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
     ios: {
-      ...config.ios,
-      appleTeamId: process.env.APPLE_TEAM_ID || config.ios.appleTeamId
-    }
+      supportsTablet: false,
+      bundleIdentifier: 'com.sknkaaa.wintrack',
+      appleTeamId: process.env.APPLE_TEAM_ID,
+      entitlements: {
+        'com.apple.security.application-groups': ['group.com.sknkaaa.wintrack']
+      },
+      infoPlist: {
+        NSCameraUsageDescription: 'カウンター写真を撮影するためにカメラを使用します。',
+        NSPhotoLibraryUsageDescription: 'カウンター写真を選択するために写真ライブラリを使用します。'
+      }
+    },
+    plugins: [
+      [
+        'expo-image-picker',
+        {
+          photosPermission: 'カウンター写真を選択するために写真ライブラリを使用します。',
+          cameraPermission: 'カウンター写真を撮影するためにカメラを使用します。'
+        }
+      ],
+      'expo-font',
+      '@bacons/apple-targets'
+    ]
   };
 };
